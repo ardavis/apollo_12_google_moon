75d37d9fb49ce9341f35d9c4309b8f3adb5b3c0a
Andrew Davis <andrew.r.davis@nasa.gov>
2013-09-09 14:05:38 -0400

    Working on adding Apollo 12 to Google Earth with Ruby KML

--- apollo12.rb
+++ apollo12.rb
@@ -1,8 +1,10 @@
 require 'ruby_kml'
 
 kml = KMLFile.new
+kml.target = 'moon'
 
 folder = KML::Folder.new(name: 'Apollo 12 Moon Landing')
+tour = KML::Tour.new(name: 'Landing Tour', description: "Andy's attempt at recreating the Apollo 12 Moon Landing within Google Earth")
 
 frames = [
 	{
@@ -22,16 +24,45 @@ frames = [
 		head: -112.1842470470624,
 		tilt: 30.08006766232498,
 		roll: 5
-	}
+	},
+  {
+    name: 'Frame 2569',
+    lng: -23.46138376727904,
+    lat: -3.024139365484832,
+    alt: 55.16999999891912,
+    head: -112.1842470470624,
+    tilt: 30.08006766232498,
+    roll: 25
+  },
+  {
+    name: 'Frame 2569',
+    lng: -23.46138376727904,
+    lat: -3.024139365484832,
+    alt: 55.16999999891912,
+    head: -112.1842470470624,
+    tilt: 30.08006766232498,
+    roll: 75
+  }
 ]
 
 frames.each do |frame|
-	puts frame[:lat]
-	folder.features << KML::Placemark.new(
-		name: frame[:name],
-		geometry: KML::Point.new(coordinates: { lat: frame[:lat], lng: frame[:lng] })
-	)	
-end	
+  camera = KML::Camera.new(latitude: frame[:lat], longitude: frame[:lng], altitude: frame[:alt], heading: frame[:head], tilt: frame[:tilt], roll: frame[:roll])
+  fly_to = KML::FlyTo.new(duration: 1, feature: camera)
+
+  tour.features << fly_to
+end
+
+
+#frames.each do |frame|
+#	puts frame[:lat]
+#	folder.features << KML::Placemark.new(
+#		name: frame[:name],
+#		geometry: KML::Point.new(coordinates: { lat: frame[:lat], lng: frame[:lng] })
+#	)
+#end
 
 kml.objects << folder
+folder.features << tour
 puts kml.render
+
+kml.save('test.kml')
